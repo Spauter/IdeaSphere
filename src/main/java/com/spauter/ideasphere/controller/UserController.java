06756c2b4c7b026762ac091768b2ea75669b2a09
32306 <3230695439@qq.com>
2025-08-19 21:34:05 +0800

    使用redis实现保持登录

--- src/main/java/com/spauter/ideasphere/controller/UserController.java
+++ src/main/java/com/spauter/ideasphere/controller/UserController.java
@@ -1,32 +1,32 @@
 package com.spauter.ideasphere.controller;
 
-import cn.hutool.crypto.digest.DigestUtil;
-import com.spauter.extra.baseentity.builder.TablePkGenerator;
-import com.spauter.extra.baseentity.searcher.ClassFieldSearcher;
 import com.spauter.extra.database.wapper.QueryWrapper;
 import com.spauter.ideasphere.entity.User;
 import com.spauter.ideasphere.service.UserService;
 import jakarta.annotation.Resource;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.SQLException;
-import java.sql.Timestamp;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
     @Resource(name = "userService")
     private UserService userService;
+
+    @Resource
+    @Qualifier("customRedisTemplate")
+    private RedisTemplate<String, Object> redisTemplate;
 
     @RequestMapping(value = "/register", name = "注册", params = {"username", "password"})
     public Map<String, Object> register(HttpServletRequest request) throws SQLException {
@@ -48,11 +48,8 @@ public class UserController {
         }
         try {
             User user = userService.register(userName, password);
-            //todo 换成JWT+Redis
-            HttpSession session = request.getSession();
             String token = UUID.randomUUID().toString();
-            session.setAttribute(token, user);
-            // RedisTemplate.opsForValue .set(token,user);
+            redisTemplate.opsForValue().set(token, user, 20, TimeUnit.MINUTES);
             map.put("code", 200);
             map.put("msg", "Register success");
             user.setPassword(null);
@@ -83,9 +80,12 @@ public class UserController {
         User user = userService.login(username, password);
         var map = new HashMap<String, Object>();
         if (user != null) {
+            String token = UUID.randomUUID().toString();
             map.put("code", 200);
             map.put("msg", "Login success");
             map.put("data", user);
+            redisTemplate.opsForValue().set(token, user, 20, TimeUnit.MINUTES);
+            map.put("token", token);
         } else {
             map.put("code", 404);
             map.put("msg", "Username or password is wrong");
@@ -95,14 +95,10 @@ public class UserController {
 
     @GetMapping(value = "/logout", name = "登出")
     public Map<String, Object> logout(HttpServletRequest request) {
-        //todo 这里使用的是JWT令牌
         String token = request.getHeader("token");
-        HttpSession session = request.getSession();
         var map = new HashMap<String, Object>();
         try {
-            //todo 换成JWT+Redis
-//            RedisTemplate.opsForValue.remove(token);
-            session.removeAttribute(token);
+            redisTemplate.delete(token);
             map.put("code", 200);
             map.put("msg", "Logout success");
         } catch (Exception e) {
@@ -113,11 +109,22 @@ public class UserController {
         return map;
     }
 
-    //todo 换成JWT+Redis
+
     @GetMapping(value = "/loginUser", name = "获取登录用户")
-    public User getLoginUser(HttpServletRequest request) {
+    public Map<String,Object> getLoginUser(HttpServletRequest request) {
         String token = request.getHeader("token");
-        HttpSession session = request.getSession();
-        return (User) session.getAttribute(token);
+        var user= (User) redisTemplate.opsForValue().get(token);
+        var map=new HashMap<String,Object>();
+        if(user==null){
+            map.put("code",404);
+            map.put("msg","Please login");
+        }else {
+            user.setPassword(null);
+            user.setIcenterPwd(null);
+            map.put("code",200);
+            map.put("msg","Get login user success");
+            map.put("data",user);
+        }
+        return map;
     }
 }
